ec782f209fa9485a0bf41dd673b94715d8107f5b
JDRoberts1 <JDRoberts1@users.noreply.github.com>
2020-06-06 18:59:45 -0500

    Update ConvertData

--- RobertsJeanai_ConvertedData/JSONConvert.cs
+++ RobertsJeanai_ConvertedData/JSONConvert.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Data;
+
 
 namespace RobertsJeanai_ConvertedData
 {
     class JSONConvert
     {
+        // Build connection method
+        void BuildConnection()
+        {
+            // set variable for ipAddress
+            string ip = "";
 
+            using (StreamReader sr = new StreamReader("c:/VFW/conn.txt"))
+            {
+                ip = sr.ReadLine();
+            }
+
+            string conString = $"Server={ip};";
+            conString += "username = dbsAdmin;";
+            conString += "password = password;";
+            conString += "";
+        }
     }
 }
